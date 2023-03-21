53cbab993f0c1b76c8aa61a753b9981ecaa321b1
AThilina97 <thilinarangaprasad@gmail.com>
2023-03-21 19:47:00 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/dep10/sas/AppInnitializer.java
@@ -0,0 +1,65 @@
+package lk.ijse.dep10.sas;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import lk.ijse.dep10.sas.db.DBConnection;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AppInnitializer extends Application {
+
+    public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new Thread(()->{
+            System.out.println("Database connection is about to close");
+            try {
+               if(!DBConnection.getInstance().getConnection().isClosed() && DBConnection.getInstance().getConnection()!=null){
+                   DBConnection.getInstance().getConnection().close();
+
+               }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }));
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        generateSchemaIfNotExist();
+
+
+    }
+
+
+
+    private void generateSchemaIfNotExist() {
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            Statement stm = connection.createStatement();
+            ResultSet rst = stm.executeQuery("SHOW TABLES ");
+            if(!rst.next()){
+                InputStream is = getClass().getResourceAsStream("/schema.sql");
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                String line;
+                StringBuilder dbScript = new StringBuilder();
+                while ((line= br.readLine())!=null){
+                    dbScript.append(line).append("\n");
+                }
+                br.close();
+                System.out.println(dbScript.toString());
+                stm.execute(dbScript.toString());
+            }else {
+                System.out.println("No table Found");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ target/classes/dummy-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO User (username, full_name, password) VALUES
+                                                     ('Dasun','Dasun Sampath',123456),
+                                                     ('Thilina','Thilina Ranga',789632);
+
+INSERT INTO Student (id, name) VALUES
+                             (1,'Chamal'),
+                             (2,'Namal'),
+                             (3,'Ruwan');
